a45f1c4b260ac542224bc62bf63264e14b713fac
Richard Cranwell <rickycranwell@gmail.com>
2021-07-19 15:28:44 -0500

    remove html samples again

--- src/projectPageModule.js
+++ src/projectPageModule.js
@@ -4,32 +4,6 @@ import { loadTask } from "./taskPageModule";
 
 //module for displaying tasks within a project
 
-//sample html structure for page
-const htmlContent=`<div id="header">
-<div>Back</div>
-<h1>Project Name</h1>
-<div>Rename</div>
-</div>
-<div class="create-button">Create New Task</div>
-<ul id="task-list">
-<li class="task">
-  <div class ="task-minimize">^</div>
-  <div>update</div>
-  <div class ="task-title">Task 1</div>
-  <div class ="task-desc">One task description</div>
-  <div>edit details</div>
-  <div>additional notes</div>
-  <div>delete</div>
-  <div>priority</div>
-  <div>deadline</div>
-  <div>add to checklist</div>
-  <li class ="check-list">
-    <ul>check1</ul>
-    <ul>check2</ul>
-  </li>
-</li>
-</ul>`;
-
 //use the current project to assemble an html page
 const loadProject = (content, project) =>{
   content.innerHTML = '';
